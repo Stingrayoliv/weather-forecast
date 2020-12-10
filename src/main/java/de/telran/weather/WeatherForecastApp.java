7aafcdf747181292faf0740a58ac11525315ac60
Stingrayoliv <64040033+Stingrayoliv@users.noreply.github.com>
2020-12-10 17:19:21 +0100

    the new project structure has been added

---
+++ src/main/java/de/telran/weather/WeatherForecastApp.java
@@ -0,0 +1,26 @@
+package de.telran.weather;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.telran.weather.service.InputOutputService;
+import de.telran.weather.service.WeatherGateway;
+import de.telran.weather.service.WeatherService;
+
+public class WeatherForecastApp {
+    private InputOutputService inputOutputService;
+    private WeatherService service;
+
+    public WeatherForecastApp(InputOutputService inputOutputService, WeatherService service) {
+        this.inputOutputService = inputOutputService;
+        this.service = service;
+    }
+
+    public void execute() throws Exception {
+        String s = inputOutputService.readValue();
+        String result = service.getWeatherByCityName(s);
+        inputOutputService.print(result);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
---
+++ src/test/java/de/telran/weather/service/WeatherGatewayTest.java
@@ -0,0 +1,29 @@
+package de.telran.weather.service;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import de.telran.weather.entity.Forecast;
+import de.telran.weather.entity.SearchResult;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
+
+class WeatherGatewayTest {
+    @Test
+    public void testFindCityByName() throws Exception {
+        WeatherGateway gateway = new WeatherGateway(new ObjectMapper());
+
+        SearchResult[] berlins = gateway.findCityByName("Berlin");
+
+        assertTrue(berlins.length == 1);
+    }
+
+    @Test
+    public void testGetWeatherByWoeid() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        WeatherGateway gateway = new WeatherGateway(mapper);
+        Forecast weatherByWoeid = gateway.getWeatherByWoeid("638242");
+    }
+}
